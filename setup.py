bc175c41a8975f1ed2ef8dd4db872c6e4211ec94
Ben Christenson <ben.christenson@xperi.com>
2017-11-01 09:19:16 -0500

    Created a setup.py in root so that remote pip install works

---
+++ setup.py
@@ -0,0 +1,40 @@
+import sys
+from os import path
+
+from setuptools import setup, find_packages
+
+package_path = '.'
+
+if sys.platform == "darwin":  # mac
+    package_path = path.join(package_path, "mac", "x86-64")
+elif sys.platform != "win":  # linux
+    package_path = path.join(package_path, "linux", "x86-64")
+elif sys.maxsize > 2 ** 32:  # 64 bit windows
+    package_path = path.join(package_path, "windows", "win64")
+else:  # 32 bit windows
+    package_path = path.join(package_path, "windows", "win32")
+
+if sys.version[0] == '3':
+    package_path = path.join(package_path, 'python3')
+else:
+    package_path = path.join(package_path, 'python2.7')
+
+setup(
+    name="pyacrcloud",
+    version="1.0.0",
+    packages=find_packages(package_path),
+    package_dir={"": package_path},
+
+    package_data={
+        '': ['*.txt', '*.rst'],
+        'acrcloud': ['*.so', '*.pyd'],
+    },
+
+    author="ACRCloud",
+    author_email="support@acrcloud.com",
+    description='Python wrapper for acrcloud libraries',
+    license='MIT',
+    keywords="ACRCLoud Python SDK",
+    url='https://github.com/acrcloud/acrcloud_sdk_python',
+    zip_safe=False,
+)
