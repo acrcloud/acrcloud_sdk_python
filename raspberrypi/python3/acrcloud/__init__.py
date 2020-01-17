3e8022a615df8b2568750c2083ae72a610f453f7
pony <383385741@qq.com>
2020-01-17 17:57:23 +0800

    add RPi aarch64

--- raspberrypi/python3/acrcloud/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python                                                                                                                                             
-#-*- coding: utf-8 -*-
-
-import os,sys
-
-sys.path.append(os.path.split(os.path.realpath(__file__))[0])
