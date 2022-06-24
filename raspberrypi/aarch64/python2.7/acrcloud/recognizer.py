686b935ddc9ba927aaef87da24fb9eb6a10c2acf
pony <383385741@qq.com>
2022-06-24 11:35:10 +0800

    update aarch64 humming fingerprint

--- raspberrypi/aarch64/python2.7/acrcloud/recognizer.py
+++ raspberrypi/aarch64/python2.7/acrcloud/recognizer.py
@@ -179,7 +179,7 @@ class ACRCloudRecognizer:
                 query_data['sample'] = acrcloud_extr_tool.create_fingerprint_by_file(file_path, start_seconds, rec_length, False, filter_e)
 
             if self.recognize_type == ACRCloudRecognizeType.ACR_OPT_REC_HUMMING or self.recognize_type == ACRCloudRecognizeType.ACR_OPT_REC_BOTH:
-                query_data['sample_hum'] = acrcloud_extr_tool.create_humming_fingerprint_by_file(file_path, start_seconds, rec_length)
+                query_data['sample_hum'] = acrcloud_extr_tool.create_humming_fingerprint_by_file(file_path, start_seconds, rec_length, 2)
 
             res = self.do_recogize(self.host, query_data, self.query_type, self.access_key, self.access_secret, self.timeout)
 
@@ -200,7 +200,7 @@ class ACRCloudRecognizer:
                 query_data['sample'] = acrcloud_extr_tool.create_fingerprint_by_filebuffer(file_buffer, start_seconds, rec_length, False)
 
             if self.recognize_type == ACRCloudRecognizeType.ACR_OPT_REC_HUMMING or self.recognize_type == ACRCloudRecognizeType.ACR_OPT_REC_BOTH:
-                query_data['sample_hum'] = acrcloud_extr_tool.create_humming_fingerprint_by_filebuffer(file_buffer, start_seconds, rec_length)
+                query_data['sample_hum'] = acrcloud_extr_tool.create_humming_fingerprint_by_filebuffer(file_buffer, start_seconds, rec_length, 2)
 
             res = self.do_recogize(self.host, query_data, self.query_type, self.access_key, self.access_secret, self.timeout)
             try:
