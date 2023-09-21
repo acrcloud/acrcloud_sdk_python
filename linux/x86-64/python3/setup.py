b4c97d1925617dabfb48075e509beb1068abbba7
Ben Christenson <contact@benchristenson.com>
2023-09-21 12:31:15 -0500

    15024 acr: replaced timeout and rebased on upstream

--- linux/x86-64/python3/setup.py
+++ linux/x86-64/python3/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name="pyacrcloud",
-    version="1.0.0.post2",
+    version="1.0.0.post3",
     packages=find_packages(),
 
     package_data={
