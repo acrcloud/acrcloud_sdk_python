c98e66a4e86c23d0cebdf01bcad9a4633f8081f1
Ben Christenson <contact@benchristenson.com>
2024-04-10 09:38:00 -0500

    16544: updated setup version, async recognizer silence value, added aarch64

--- mac/aarch64/python3/setup.py
+++ mac/aarch64/python3/setup.py
@@ -3,14 +3,16 @@
 from setuptools import setup, find_packages
 setup(
     name="pyacrcloud",
-    version="1.0.0",
+    version="1.0.0.post4",
     packages=find_packages(),
 
     package_data={
         '': ['*.txt', '*.rst'],
         'acrcloud': ['*.so'],
     },
-
+    install_requires=[
+        'async_timeout',
+    ],
     author="ACRCloud",
     author_email="support@acrcloud.com",
     description='Python wrapper for acrcloud libraries',
